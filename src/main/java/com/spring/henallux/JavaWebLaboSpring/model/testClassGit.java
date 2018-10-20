1f6d1b686c72ff64afe1628356e751c7c9be12f1
Samuel Decruynaere <samuel.decruynaere@gmail.com>
2018-10-20 14:39:10 +0200

    get et set et constructor

--- src/main/java/com/spring/henallux/JavaWebLaboSpring/model/testClassGit.java
+++ src/main/java/com/spring/henallux/JavaWebLaboSpring/model/testClassGit.java
@@ -6,4 +6,43 @@ public class testClassGit {
     private String pass;
     private int somme;
 
+
+    public testClassGit(String texte, int numberPair, String pass, int somme) {
+        texte = texte;
+        numberPair = numberPair;
+        pass = pass;
+        somme = somme;
+    }
+
+    public String getTexte() {
+        return texte;
+    }
+
+    public void setTexte(String texte) {
+        this.texte = texte;
+    }
+
+    public int getNumberPair() {
+        return numberPair;
+    }
+
+    public void setNumberPair(int numberPair) {
+        this.numberPair = numberPair;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public int getSomme() {
+        return somme;
+    }
+
+    public void setSomme(int somme) {
+        this.somme = somme;
+    }
 }
