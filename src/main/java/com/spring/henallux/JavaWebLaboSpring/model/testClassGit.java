e57c9ccf498627ecf3744e9905c77ac12071f97d
Samuel Decruynaere <samuel.decruynaere@gmail.com>
2018-10-20 14:17:44 +0200

    commit 4 attributs pass et somme

--- src/main/java/com/spring/henallux/JavaWebLaboSpring/model/testClassGit.java
+++ src/main/java/com/spring/henallux/JavaWebLaboSpring/model/testClassGit.java
@@ -3,5 +3,7 @@ package com.spring.henallux.JavaWebLaboSpring.model;
 public class testClassGit {
     private String texte;
     private int numberPair;
+    private String pass;
+    private int somme;
 
 }
